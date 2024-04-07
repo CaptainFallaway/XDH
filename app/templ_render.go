60252d1f1cd2b40d82a0a79bbc29a98b47e6c738
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-04-07 03:38:10 +0200

    Add templ_render.go for rendering templates

---
+++ app/templ_render.go
@@ -0,0 +1,25 @@
+package app
+
+import (
+	"context"
+
+	"github.com/a-h/templ"
+)
+
+type Component struct {
+	Html string
+}
+
+// Write is a method that satisfies the io.Writer interface
+func (c *Component) Write(p []byte) (int, error) {
+	c.Html = string(p)
+	return len(p), nil
+}
+
+// You should only care about this function for rendering the templates
+// Returns the rendered HTML as a string for the swapping in the frontend
+func Render(comp templ.Component) string {
+	var component Component
+	comp.Render(context.Background(), &component)
+	return component.Html
+}
