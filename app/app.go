1578a5eb3c7869b60ea687406ed97372d92f48d6
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-04-07 03:37:41 +0200

    Remove unused imports and methods

--- app/app.go
+++ app/app.go
@@ -1,12 +1,7 @@
 package app
 
 import (
-	"XRF/templates"
 	"context"
-	"fmt"
-	"strings"
-
-	"github.com/a-h/templ"
 )
 
 // App struct
@@ -23,35 +18,4 @@ func NewApp() *App {
 // so we can call the runtime methods
 func (a *App) Startup(ctx context.Context) {
 	a.ctx = ctx
-}
-
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
-}
-
-type Component struct {
-	Html string
-}
-
-// Write is a method that satisfies the io.Writer interface
-func (c *Component) Write(p []byte) (int, error) {
-	c.Html = string(p)
-
-	return len(p), nil
-}
-
-func render(comp templ.Component) string {
-	var component Component
-	comp.Render(context.Background(), &component)
-	return component.Html
-
-}
-
-func (a *App) GetHello() string {
-	sb := strings.Builder{}
-	for i := 0; i < 1000; i++ {
-		sb.WriteString(render(templates.Hello()))
-	}
-	return sb.String()
 }
