b10e3569da3d320de4f2dc318a305a30e5c437ac
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-04-07 03:40:22 +0200

    Implemented own lightweight ViewModel class

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,17 +1,44 @@
-import {GetHello} from '../wailsjs/go/app/App';
+/// This is supposed to be like a lightweight ViewModel
 
-window.render = function() {
-    // Call App.GetHello()
-    try {
-        GetHello()
-            .then((result) => {
-                // Update result with data back from App.GetHello()
-                document.querySelector('#models').innerHTML = result;
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    } catch (err) {
-        console.error(err);
+import {GetModels} from '../wailsjs/go/app/ModelInterface';
+
+// Helper function to bind events to elements
+function bindEvent(id, event, callback) {
+    let element = document.querySelector(id);
+    if (!element) {
+        console.error("Element not found: " + id);
+        return;
+    }
+    element.addEventListener(event, callback);
+}
+
+function swapInner(id, content) {
+    let element = document.getElementById(id);
+    if (!element) {
+        console.error("Element not found: " + id);
+        return;
+    }
+    element.innerHTML = content;
+}
+
+export class ViewModel {
+    constructor() {
+        // Bindings here
+
+        bindEvent("#test", "click", this.render);
+        bindEvent("#model", "click", this.render);
+    }
+
+    // Just Testing
+    render() {
+        GetModels()
+        .then((result) => {
+            swapInner("models", result)
+        })
+        .catch((err) => {
+            console.log(err);
+        });
     }
 }
+ 
+new ViewModel()
