3ca9c5825eeacd09f202ba640fcd97bbff2f8d3b
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-06-02 19:13:09 +0200

    mend
    fix: removed viewmodel reference, see @e9bec711fb72e67f54551e50ec57c826a120f89e

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,16 @@
+import { OpenFileDialog } from "../wailsjs/go/app/App.js";
+
 const dropArea = document.getElementById('drop-area');
 
-function handleFile(files) {
-    console.log(files);
-}
-
-dropArea.addEventListener('change', (e) => {
-    handleFile(e.target.files);
-});
-
-dropArea.addEventListener('dragover', (e) => { e.preventDefault() });
-
-dropArea.addEventListener('drop', (e) => {
+dropArea.addEventListener('click', async (e) => {
     e.preventDefault();
-    handleFile(e.dataTransfer.files);
+    await OpenFileDialog();
 });
+
+// THIS WILL BE IMPLEMENTED IN WAILS VERSION 3
+// dropArea.addEventListener('dragover', (e) => { e.preventDefault() });
+
+// dropArea.addEventListener('drop', (e) => {
+//     e.preventDefault();
+//     handleFile(e.dataTransfer.files);
+// });
