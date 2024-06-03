e073975219648d51af93779cc1d9615981b07533
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-06-03 17:25:05 +0200

    fix: Drop area and loading the models not work

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,30 @@
-import { OpenFileDialog } from "../wailsjs/go/app/App.js";
+import { GetModels, OpenFileDialog, GetDropArea } from "../wailsjs/go/app/App.js";
+import { EventsOn } from "../wailsjs/runtime/runtime.js";
 
-const dropArea = document.getElementById('drop-area');
+const content = document.getElementById('content');
+var sortingMetal = "Sn";
 
-dropArea.addEventListener('click', async (e) => {
-    e.preventDefault();
-    await OpenFileDialog();
+EventsOn('modelLoaded', async () => loadModels())
+loadDropArea()
+
+document.querySelectorAll('input[name="sortingMetal"]').forEach((radio) => {
+    radio.addEventListener('change', async (e) => {
+        sortingMetal = e.target.id
+        await loadModels()
+    })
 });
+
+async function loadDropArea() {
+    content.innerHTML = await GetDropArea()
+    document.getElementById('drop-area').addEventListener('click', async (e) => {
+        e.preventDefault();
+        await OpenFileDialog();
+    });
+}
+
+async function loadModels() {
+    content.innerHTML = await GetModels(sortingMetal)
+}
 
 // THIS WILL BE IMPLEMENTED IN WAILS VERSION 3
 // dropArea.addEventListener('dragover', (e) => { e.preventDefault() });
