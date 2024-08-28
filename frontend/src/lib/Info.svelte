d28a3d872dc14592c033cad7071c8e520b672107
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-08-28 23:04:19 +0200

    Component for showing info before expandation

---
+++ frontend/src/lib/Info.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let desc = "";
+    export let info = "";
+</script>
+
+<div class="justify-start">
+    <small class="desc">{desc}</small>
+    <p>{info}</p>
+</div>
+
+<style>
+    .desc {
+        /* font-weight: bold; */
+        color: var(--secondary)
+    }
+</style>
