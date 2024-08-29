b7b898ff52a6fdacd16750385640a2108886bbb2
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-08-29 13:52:26 +0200

    feat: added a table component

---
+++ frontend/src/lib/Table.svelte
@@ -0,0 +1,32 @@
+<script>
+    import * as Table from "$lib/components/ui/table/index.js";
+
+    export let scans = [];
+
+    if (scans == null) {
+        scans = [];
+    }
+</script>
+
+<Table.Root>
+    <Table.Header>
+        <Table.Row>
+            <Table.Head>Reading No</Table.Head>
+            <Table.Head>Duration</Table.Head>
+            <Table.Head>Time</Table.Head>
+            <Table.Head>Pb</Table.Head>
+            <Table.Head>Sn</Table.Head>
+        </Table.Row>
+    </Table.Header>
+    <Table.Body>
+        {#each scans as scan}
+            <Table.Row>
+                <Table.Cell>{scan.reading}</Table.Cell>
+                <Table.Cell>{scan.duration}</Table.Cell>
+                <Table.Cell>{scan.time}</Table.Cell>
+                <Table.Cell>{scan.pb.value}</Table.Cell>
+                <Table.Cell>{scan.sn.value}</Table.Cell>
+            </Table.Row>
+        {/each}
+    </Table.Body>
+</Table.Root>
