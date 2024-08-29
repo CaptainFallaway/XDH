675b8e6ac77272c8318d508f4da76fc2f76abbb5
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-08-29 23:28:37 +0200

    fix: fade transition on expandation

--- frontend/src/lib/Model.svelte
+++ frontend/src/lib/Model.svelte
@@ -1,6 +1,6 @@
 <script>
     import { cubicOut } from "svelte/easing";
-    import { fly, slide } from "svelte/transition";
+    import { fly, slide, fade } from "svelte/transition";
 
     import { toggleValue } from "$lib/stores.js";
     // import { Badge } from "$lib/components/ui/badge/";
@@ -47,6 +47,8 @@
             toggleShow();
         }
     }
+
+    let bombo = 'Valid';
 </script>
 
 <!-- shadow-red-500 shadow-yellow-500 shadow-green-500 (Not used right now) -->
@@ -58,20 +60,25 @@
     on:click={toggleShow}
     on:keydown={handleKeyPress}
     tabindex="0"
-    transition:fly={{ y: 200, duration: 500 }}
-    class="border-2 rounded-lg right-0 left-0 m-5 content-center shadow-lg border-l-8 border-l-{statusColor}-500"
+    in:fly|global={{ delay: 200, y: 200, duration: 200 }}
+    out:fade|global={{ duration: 200 }}
+    class="border-2 rounded-lg right-0 left-0 m-5 content-center shadow-xl border-l-4 border-l-{statusColor}-500"
 >
     <div aria-label="info" class="flex gap-10 m-8 content-center">
         <Info top="Boat ID" bottom={model.boatID} />
         <Info top="Operators" bottom={model.operators} />
         <Info top="First Date" bottom={model.firstDate} />
         <Info top="Last Date" bottom={model.lastDate} />
-        <Info top="Units" bottom={model.unit} />
+        <!-- <Info top="Unit" bottom={model.unit} /> -->
         <div class="w-full flex items-center justify-end">
             {#if show}
-                <ChevronDown size={48} />
+                <div in:fade|global>
+                    <ChevronDown size={48} />
+                </div>
             {:else}
-                <ChevronUp size={48} />
+                <div in:fade|global>
+                    <ChevronUp size={48} />
+                </div>
             {/if}
         </div>
     </div>
@@ -79,31 +86,39 @@
         <Separator />
         <div
             class="m-2 flex"
-            in:slide={{ ...expandMotion }}
-            out:slide={{ ...expandMotion }}
+            transition:slide|global={{ ...expandMotion }}
         >
             <div
-                class="w-full mx-5 mb-5 flex flex-col"
+                class="w-full mx-5 mb-5 flex flex-col cursor-default"
                 role="button"
                 tabindex="0"
-                on:click={(e) => e.stopPropagation()}
-                on:keydown={(e) => e.stopPropagation()}
+                on:click|stopPropagation
+                on:keydown|stopPropagation
+                transition:fade|global
             >
                 <div class="w-full flex content-center justify-center gap-8">
                     <Info top="Total Scans" bottom={model.totalScans} />
                     <Info top="Valid Scans" bottom={model.validScans} />
                     <Info top="Invalid Scans" bottom={model.invalidScans} />
                 </div>
-                <Tabs.Root class="w-full">
+                <Tabs.Root bind:value={bombo} class="w-full">
                     <Tabs.List>
                         <Tabs.Trigger value="Valid">Valid</Tabs.Trigger>
                         <Tabs.Trigger value="Invalid">Invalid</Tabs.Trigger>
                     </Tabs.List>
                     <Tabs.Content value="Valid">
-                        <Table scans={model.scans} />
+                        {#key bombo}
+                            <div in:fade|global>
+                                <Table scans={model.scans} />
+                            </div>
+                        {/key}
                     </Tabs.Content>
                     <Tabs.Content value="Invalid">
-                        <Table scans={model.invalidScans} />
+                        {#key bombo}
+                            <div in:fade|global>
+                                <Table scans={model.invalidScans} />
+                            </div>
+                        {/key}
                     </Tabs.Content>
                 </Tabs.Root>
             </div>
