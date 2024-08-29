4e3ebe3fea73859cd52af823710d1a1b32a4aa86
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-08-29 13:55:21 +0200

    Further progress on the recreation

--- frontend/src/lib/Model.svelte
+++ frontend/src/lib/Model.svelte
@@ -3,32 +3,36 @@
     import { fly, slide } from "svelte/transition";
 
     import { toggleValue } from "$lib/stores.js";
-    import { Badge } from "$lib/components/ui/badge/";
+    // import { Badge } from "$lib/components/ui/badge/";
+    import { Separator } from "$lib/components/ui/separator";
     import Info from "$lib/Info.svelte";
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+    import Table from "$lib/Table.svelte";
+    import { ChevronUp, ChevronDown } from "lucide-svelte";
 
-    export let id = '0';
+    export let id = "0";
     export let model = {};
 
     const expandMotion = {
         duration: 500,
         easing: cubicOut,
-    }
+    };
 
-    let status;
+    // let status;
     let statusColor;
 
     switch (model.violations[$toggleValue]) {
         case 0:
-            status = "safe"
-            statusColor = "green"
+            // status = "safe";
+            statusColor = "green";
             break;
         case 1:
-            status = "warning"
-            statusColor = "yellow"
+            // status = "warning";
+            statusColor = "yellow";
             break;
         default:
-            status = "danger"
-            statusColor = "red"
+            // status = "danger";
+            statusColor = "red";
             break;
     }
 
@@ -45,18 +49,64 @@
     }
 </script>
 
-<div {id} role="button" on:click={toggleShow} on:keydown={handleKeyPress} tabindex="0" transition:fly={{y: 200, duration: 500}} class="border-2 rounded-lg right-0 left-0 m-3">
-    <div aria-label="info" class="flex gap-4 m-2 content-center">
-        <div class="content-center">
-            <Badge {id} style="background: {statusColor}">
-                <div class="w-8 h-5"/>
-            </Badge>
+<!-- shadow-red-500 shadow-yellow-500 shadow-green-500 (Not used right now) -->
+<!-- border-l-red-500 border-l-yellow-500 border-l-green-500 -->
+
+<div
+    {id}
+    role="button"
+    on:click={toggleShow}
+    on:keydown={handleKeyPress}
+    tabindex="0"
+    transition:fly={{ y: 200, duration: 500 }}
+    class="border-2 rounded-lg right-0 left-0 m-5 content-center shadow-lg border-l-8 border-l-{statusColor}-500"
+>
+    <div aria-label="info" class="flex gap-10 m-8 content-center">
+        <Info top="Boat ID" bottom={model.boatID} />
+        <Info top="Operators" bottom={model.operators} />
+        <Info top="First Date" bottom={model.firstDate} />
+        <Info top="Last Date" bottom={model.lastDate} />
+        <Info top="Units" bottom={model.unit} />
+        <div class="w-full flex items-center justify-end">
+            {#if show}
+                <ChevronDown size={48} />
+            {:else}
+                <ChevronUp size={48} />
+            {/if}
         </div>
-        <Info desc="Index" info={model.index} />
     </div>
     {#if show}
-        <div class="m-2" in:slide={{...expandMotion}} out:slide={{...expandMotion}}>
-            <div class="w-full h-[100px] rounded-lg border-2"></div>
+        <Separator />
+        <div
+            class="m-2 flex"
+            in:slide={{ ...expandMotion }}
+            out:slide={{ ...expandMotion }}
+        >
+            <div
+                class="w-full mx-5 mb-5 flex flex-col"
+                role="button"
+                tabindex="0"
+                on:click={(e) => e.stopPropagation()}
+                on:keydown={(e) => e.stopPropagation()}
+            >
+                <div class="w-full flex content-center justify-center gap-8">
+                    <Info top="Total Scans" bottom={model.totalScans} />
+                    <Info top="Valid Scans" bottom={model.validScans} />
+                    <Info top="Invalid Scans" bottom={model.invalidScans} />
+                </div>
+                <Tabs.Root class="w-full">
+                    <Tabs.List>
+                        <Tabs.Trigger value="Valid">Valid</Tabs.Trigger>
+                        <Tabs.Trigger value="Invalid">Invalid</Tabs.Trigger>
+                    </Tabs.List>
+                    <Tabs.Content value="Valid">
+                        <Table scans={model.scans} />
+                    </Tabs.Content>
+                    <Tabs.Content value="Invalid">
+                        <Table scans={model.invalidScans} />
+                    </Tabs.Content>
+                </Tabs.Root>
+            </div>
         </div>
     {/if}
 </div>
