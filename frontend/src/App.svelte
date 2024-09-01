7c0aa132d695e0e46223b572d244db2106bf1736
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-09-02 00:43:34 +0200

    feat: loading indicator & moved `GetModels` call to the await block

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -7,22 +7,48 @@
     import Model from "$lib/Model.svelte";
     import MenuBar from "$lib/MenuBar.svelte";
 
-    import * as wails from "$lib/wailsjs/go/app/App.js";
-
-    let modelListPromise;
-    $: modelListPromise = wails.GetModels($toggleValue);
+    import * as app from "$lib/wailsjs/go/app/App.js";
 </script>
 
 <ModeWatcher />
 
 <MenuBar />
 
-<!-- svelte-ignore empty-block -->
-<!-- in the future i want to add a loading icon -->
-{#await modelListPromise then modelList}
+{#await app.GetModels($toggleValue)}
+    <div class="w-full h-screen flex justify-center">
+        <div class="loader m-auto bottom-[80px]"></div>
+    </div>
+{:then modelList}
     {#each modelList as model}
         <Model id={model.index.toString()} {model} />
     {/each}
 {:catch error}
     <p>{error.message}</p>
 {/await}
+
+<style>
+.loader {
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background-color: #fff;
+  box-shadow: 32px 0 #fff, -32px 0 #fff;
+  position: relative;
+  animation: flash 0.5s ease-out infinite alternate;
+}
+
+@keyframes flash {
+  0% {
+    background-color: #FFF2;
+    box-shadow: 32px 0 #FFF2, -32px 0 #FFF;
+  }
+  50% {
+    background-color: #FFF;
+    box-shadow: 32px 0 #FFF2, -32px 0 #FFF2;
+  }
+  100% {
+    background-color: #FFF2;
+    box-shadow: 32px 0 #FFF, -32px 0 #FFF2;
+  }
+}
+</style>
