83860faaf14c1b29dca2fb6778b825ca528bfb8f
CaptainFallaway <alexander.wollmer97@gmail.com>
2024-09-02 14:12:22 +0200

    fix & refactor: making a null check and returning empty list instead of svelte throwing error

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -8,15 +8,28 @@
     import MenuBar from "$lib/MenuBar.svelte";
 
     import * as app from "$lib/wailsjs/go/app/App.js";
+
+    async function getModels(togglevalue) {
+        if (togglevalue === "") {
+            return [];
+        }
+
+        const models = await app.GetModels(togglevalue);
+
+        return models == null ? [] : models;
+    }
 </script>
 
 <ModeWatcher />
 
 <MenuBar />
 
-{#await app.GetModels($toggleValue)}
-    <div style="--main: var(--{$mode}-main); --secon: var(--{$mode}-secon)" class="w-full h-screen flex justify-center">
-        <div  class="loader m-auto bottom-[80px]"></div>
+{#await getModels($toggleValue)}
+    <div
+        style="--main: var(--{$mode}-main); --secon: var(--{$mode}-secon)"
+        class="w-full h-screen flex justify-center"
+    >
+        <div class="loader m-auto bottom-[80px]"></div>
     </div>
 {:then modelList}
     {#each modelList as model}
@@ -27,35 +40,43 @@
 {/await}
 
 <style>
-:root {
-    --dark-main: #fff;
-    --dark-secon: #fff2;
-    --light-main: #000;
-    --light-secon: #0002;
-}
-.loader {
-  
-  width: 16px;
-  height: 16px;
-  border-radius: 50%;
-  background-color: var(--main);
-  box-shadow: 32px 0 var(--main), -32px 0 var(--main);
-  position: relative;
-  animation: flash 0.5s ease-out infinite alternate;
-}
+    :root {
+        --dark-main: #fff;
+        --dark-secon: #fff2;
+        --light-main: #000;
+        --light-secon: #0002;
+    }
 
-@keyframes flash {
-  0% {
-    background-color: var(--secon);
-    box-shadow: 32px 0 var(--secon), -32px 0 var(--main);
-  }
-  50% {
-    background-color: var(--main);
-    box-shadow: 32px 0 var(--secon), -32px 0 var(--secon);
-  }
-  100% {
-    background-color: var(--secon);
-    box-shadow: 32px 0 var(--main), -32px 0 var(--secon);
-  }
-}
+    .loader {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+        background-color: var(--main);
+        box-shadow:
+            32px 0 var(--main),
+            -32px 0 var(--main);
+        position: relative;
+        animation: flash 0.5s ease-out infinite alternate;
+    }
+
+    @keyframes flash {
+        0% {
+            background-color: var(--secon);
+            box-shadow:
+                32px 0 var(--secon),
+                -32px 0 var(--main);
+        }
+        50% {
+            background-color: var(--main);
+            box-shadow:
+                32px 0 var(--secon),
+                -32px 0 var(--secon);
+        }
+        100% {
+            background-color: var(--secon);
+            box-shadow:
+                32px 0 var(--main),
+                -32px 0 var(--secon);
+        }
+    }
 </style>
