b1723c937e9757fb8dea3ca1e7510661105162d6
CaptainFallaway <alexander.wollmer97@gmail.com>
2025-05-15 01:04:18 +0200

    Added a little note

--- frontend/src/components/surveyModal/newSurveryBtn.tsx
+++ frontend/src/components/surveyModal/newSurveryBtn.tsx
@@ -4,6 +4,7 @@ import type { NewSurveyBtnProps } from './types';
 const modalId = 'new-survey-modal';
 
 export default function NewSurveyBtn({ onCancel, onSubmit, children, ...props }: NewSurveyBtnProps) {
+  // This is probably not the best way to do this, but it works for now
   const openModal = () => {
     const modal = document.getElementById(modalId) as HTMLDialogElement;
     modal?.showModal();
